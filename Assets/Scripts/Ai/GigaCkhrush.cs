401dacbfa775897325ef121ecdaa66abc3491e27
Argnamen <argnamen@gmail.com>
2025-10-03 15:31:43 +1000

    Update Enemy Players

---
+++ Assets/Scripts/Ai/GigaCkhrush.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GigaCkhrush : IEnemyPlayer
+{
+    public PlayerData PlayerData => throw new System.NotImplementedException();
+}
--- Assets/Scripts/Ai/IEnemyPlayer.cs
+++ Assets/Scripts/Ai/IEnemyPlayer.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject.SpaceFighter;
 
 public interface IEnemyPlayer
 {
-
+    public PlayerData PlayerData { get; }
 }
